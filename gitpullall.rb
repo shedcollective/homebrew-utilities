dee0f55bb521444ed2a89684c86a8fb1375f03cc
Pablo de la Peña <p@shedcollective.org>
2015-04-27 09:38:30 +0100

    Updated/corrected forumlas

--- gitpullall.rb
+++ gitpullall.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Gitpullall < Formula
   homepage "https://github.com/shedhq/gitpullall"
-  url "https://github.com/nailsapp/command-line-tool/archive/0.2.2.tar.gz"
-  sha1 "7a18389565c0e331b08b9d15d8de191fd5621c85"
+  url "https://github.com/shedhq/gitpullall/archive/1.0.0.tar.gz"
+  sha1 "075b19e8f743ba9e7027abec28017eeed526a7d5"
 
   depends_on "git"
 
--- serverbackup.rb
+++ serverbackup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Serverbackup < Formula
   homepage "https://github.com/shedhq/serverbackup"
   url "https://github.com/shedhq/serverbackup/archive/1.1.0.tar.gz"
-  sha1 "0ae1c13730c87ca19322821a0b3b7d18"
+  sha1 "7f2bfe48641b83c85f158c09291d7ec84ed6f10b"
 
   depends_on "s3cmd"
 
