fdaf47e91c1b8b575bf00a694c8051145d2c4b8c
Pablo de la Pena <p@shedcollective.org>
2015-08-22 11:55:24 +0100

    Corrected SHA1 hash

--- serverbackup.rb
+++ serverbackup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Serverbackup < Formula
   homepage "https://github.com/shedhq/serverbackup"
   url "https://github.com/shedhq/serverbackup/archive/3.0.1.tar.gz"
-  sha1 "c76b34f34f92745433970e2352a6d0b9"
+  sha1 "31dad25f69a4bbcfc555a5da9ef45ee6dc5fa584"
 
   depends_on "s3cmd"
 
