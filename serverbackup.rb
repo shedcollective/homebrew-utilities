08604b4b121acbe1a8cca3740142e3b26dd4d325
Pablo de la Pena <p@shedcollective.org>
2015-08-22 11:51:52 +0100

    Updated Formula
    serverbackup › 3.0.1

--- serverbackup.rb
+++ serverbackup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Serverbackup < Formula
   homepage "https://github.com/shedhq/serverbackup"
-  url "https://github.com/shedhq/serverbackup/archive/2.0.0.tar.gz"
-  sha1 "65c99acde508931d96ebbca66798d28de2b754cf"
+  url "https://github.com/shedhq/serverbackup/archive/3.0.1.tar.gz"
+  sha1 "c76b34f34f92745433970e2352a6d0b9"
 
   depends_on "s3cmd"
 
