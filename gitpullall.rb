b4884ec3184d57bb5ec84651933f0a611eb167eb
Pablo de la Peña <p@shedcollective.org>
2015-11-08 13:12:51 +0000

    Updated URL's - renamed github repository

--- gitpullall.rb
+++ gitpullall.rb
@@ -1,8 +1,8 @@
 require "formula"
 
 class Gitpullall < Formula
-  homepage "https://github.com/shedhq/gitpullall"
-  url "https://github.com/shedhq/gitpullall/archive/1.0.1.tar.gz"
+  homepage "https://github.com/shedcollective/gitpullall"
+  url "https://github.com/shedcollective/gitpullall/archive/1.0.1.tar.gz"
   sha1 "f55a6a1c6ce6d4c257e588cab1038e85b515a354"
 
   depends_on "git"
--- serverbackup.rb
+++ serverbackup.rb
@@ -1,8 +1,8 @@
 require "formula"
 
 class Serverbackup < Formula
-  homepage "https://github.com/shedhq/serverbackup"
-  url "https://github.com/shedhq/serverbackup/archive/3.0.1.tar.gz"
+  homepage "https://github.com/shedcollective/serverbackup"
+  url "https://github.com/shedcollective/serverbackup/archive/3.0.1.tar.gz"
   sha1 "31dad25f69a4bbcfc555a5da9ef45ee6dc5fa584"
 
   depends_on "s3cmd"
