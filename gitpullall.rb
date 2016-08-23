3992121d4526eb849f3302f84af280b92a7f1615
Pablo de la Peña <p@shedcollective.org>
2016-08-23 10:09:22 +0100

    Updated formula to use sha256

--- gitpullall.rb
+++ gitpullall.rb
@@ -3,7 +3,7 @@ require "formula"
 class Gitpullall < Formula
   homepage "https://github.com/shedcollective/gitpullall"
   url "https://github.com/shedcollective/gitpullall/archive/1.0.1.tar.gz"
-  sha1 "f55a6a1c6ce6d4c257e588cab1038e85b515a354"
+  sha256 "bb8f345f81e4d2eceb4ecedeee65c53faae536125d3b6cc65cdb678bbeac367f"
 
   depends_on "git"
 
--- serverbackup.rb
+++ serverbackup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Serverbackup < Formula
   homepage "https://github.com/shedcollective/serverbackup"
   url "https://github.com/shedcollective/serverbackup/archive/3.0.1.tar.gz"
-  sha1 "31dad25f69a4bbcfc555a5da9ef45ee6dc5fa584"
+  sha256 "425d87aebc005f346d96a170934301a537734a6138249ea1288e570596c331b7"
 
   depends_on "s3cmd"
 
