2175b439bef086d40412c817c8d406f82d168a38
Pablo de la Peña <p@shedcollective.org>
2019-02-03 23:15:08 +0000

    Bumped version for `shed` to 1.0.11 and updated checksum to match

--- shed.rb
+++ shed.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Shed < Formula
   homepage "https://shedcollective.com"
-  url "https://github.com/shedcollective/shed-cli-tool/archive/1.0.10.tar.gz"
+  url "https://github.com/shedcollective/shed-cli-tool/archive/1.0.11.tar.gz"
 
   # Generate hash of the above file
-  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.0.10.tar.gz | shasum -a 256
-  sha256 "1693c7dd3c3d9480c5349590a7138e03301882d4a2456fb1560e48f4034c598e"
+  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.0.11.tar.gz | shasum -a 256
+  sha256 "54e4872d9017ae28d50349cccc1695e39ce2b55bdddb3565ea57b75e3a39908a"
 
   def install
     bin.install Dir["dist/*"]
