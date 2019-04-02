38270b33aaace616e90ed6ebc45e42d720adb547
Pablo de la Peña <p@shedcollective.org>
2019-04-02 10:40:45 +0100

    fix: Xorrected incorrect checksum

--- shed.rb
+++ shed.rb
@@ -5,8 +5,8 @@ class Shed < Formula
   url "https://github.com/shedcollective/shed-cli-tool/archive/1.1.0.tar.gz"
 
   # Generate hash of the above file
-  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.1.-.tar.gz | shasum -a 256
-  sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
+  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.1.0.tar.gz | shasum -a 256
+  sha256 "6ccdd35c5569ea9a57cf243a98abd0ddefcea2238b0974b9964676c2aad5cbe1"
 
   def install
     bin.install Dir["dist/*"]
