36054155a68709c78d7ef425e55e1ac6e7cce953
Pablo de la Peña <p@shedcollective.org>
2019-05-28 10:55:37 +0100

    Adding s3cmd and mysql-client as dependencies of the tool

--- shed.rb
+++ shed.rb
@@ -8,6 +8,10 @@ class Shed < Formula
   # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.2.0.tar.gz | shasum -a 256
   sha256 "6713acf8246c59ff88ac47463dd0f100df11cc64715014fce225df263df5b097"
 
+  # Specify dependencies
+  depends_on "s3cmd"
+  depends_on "mysql-client"
+
   def install
     bin.install Dir["dist/*"]
   end
