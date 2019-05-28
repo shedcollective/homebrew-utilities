52429e5a91aa080139376a765a79b490134f432d
Pablo de la Peña <p@shedcollective.org>
2019-05-28 11:15:57 +0100

    Bumped version for `shed` to 1.2.2 and updated checksum to match

--- shed.rb
+++ shed.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Shed < Formula
   homepage "https://shedcollective.com"
-  url "https://github.com/shedcollective/shed-cli-tool/archive/1.2.1.tar.gz"
+  url "https://github.com/shedcollective/shed-cli-tool/archive/1.2.2.tar.gz"
 
   # Generate hash of the above file
-  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.2.1.tar.gz | shasum -a 256
-  sha256 "d4f2698b345cf53d6d1bb9308120f135dcbed72d92164972cde6068d2d3fa929"
+  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.2.2.tar.gz | shasum -a 256
+  sha256 "a141bcb7f8c70093d3e20468c0148f71706c1473c87cde174abf917de8166f50"
 
   # Specify dependencies
   depends_on "s3cmd"
