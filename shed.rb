698d6515eff1f39722f0b8acb336e77a821bc766
Pablo de la Peña <p@shedcollective.org>
2020-10-20 16:03:47 +0100

    Bumped version for `shed` to 1.4.6 and updated checksum to match

--- shed.rb
+++ shed.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Shed < Formula
   homepage "https://shedcollective.com"
-  url "https://github.com/shedcollective/shed-cli-tool/archive/1.4.5.tar.gz"
+  url "https://github.com/shedcollective/shed-cli-tool/archive/1.4.6.tar.gz"
 
   # Generate hash of the above file
-  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.4.5.tar.gz | shasum -a 256
-  sha256 "50e673b5361dcc41bac84e656a9adb7ea609fdec1bdcd1b64f53bbf615bcab7e"
+  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.4.6.tar.gz | shasum -a 256
+  sha256 "1087d3649efceb3d86985909f142cf5a006f8a0d11415422cba2fa489e97365e"
 
   # Specify dependencies
   depends_on "php"
