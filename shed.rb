87f5d987b086822e31085fd84655d3353aaaa4b9
Pablo de la Peña <p@shedcollective.org>
2023-04-11 11:33:12 +0200

    chore: Update comment

--- shed.rb
+++ shed.rb
@@ -4,8 +4,8 @@ class Shed < Formula
   homepage "https://shedcollective.com"
   url "https://github.com/shedcollective/shed-cli-tool/archive/1.12.1.tar.gz"
 
-  # Generate hash of the above file
-  # curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.12.0.tar.gz | shasum -a 256
+  # Generate hash of the above file and put onto clipboard
+  # printf $(curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.12.1.tar.gz | shasum -a 256 | cut -c 1-64) | pbcopy
   sha256 "3177475efaccb0e08e96ee494fbd7432165c750e47a435075670c09be5865c91"
 
   # Specify dependencies
