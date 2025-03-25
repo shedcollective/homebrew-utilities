5dc6cd04514d98610aaf1ddd3040525b95a17cdd
Pablo de la Peña <p@shedcollective.org>
2025-03-25 11:47:18 +0000

    Bumped version for `shed` to 1.15.0 and updated checksum to match
    Also adds description, enforces  minimum php8.1 requirement, and adds a test

--- shed.rb
+++ shed.rb
@@ -1,19 +1,25 @@
 require "formula"
 
 class Shed < Formula
+  desc "A tool which makes life at Shed easier."
   homepage "https://shedcollective.com"
-  url "https://github.com/shedcollective/shed-cli-tool/archive/1.15.0.tar.gz"
+  url "https://github.com/shedcollective/shed-cli-tool/archive/1.16.0.tar.gz"
 
   # Generate hash of the above file and put onto clipboard
-  # printf $(curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.15.0.tar.gz | shasum -a 256 | cut -c 1-64) | pbcopy
-  sha256 "ef4e6b5b666224ca5b5d1ca0c760b00a79920c10036b27c509ca4b1c1c76876c"
+  # printf $(curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.16.0.tar.gz | shasum -a 256 | cut -c 1-64) | pbcopy
+  sha256 "523daf007fe58ce4f8289fa9b3c677b80da331d5ccb27e5f312d11bbe1f11910"
 
   # Specify dependencies
-  depends_on "php"
+  depends_on "php@8.1"
   depends_on "s3cmd"
   depends_on "mysql-client"
 
   def install
     bin.install Dir["dist/*"]
   end
+
+  test do
+    assert_match "Shed Command Line Tool 1.16.0", shell_output("#{bin}/shed --version")
+  end
+
 end
