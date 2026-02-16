d555a50dc2e9a41643c1dd192e4f90fb995b62c4
Pablo de la Peña <p@shedcollective.org>
2026-02-16 10:31:06 +0000

    Bumped version for `shed` to 1.19.2 and updated checksum to match

--- shed.rb
+++ shed.rb
@@ -3,11 +3,11 @@ require "formula"
 class Shed < Formula
   desc "A tool which makes life at Shed easier."
   homepage "https://shedcollective.com"
-  url "https://github.com/shedcollective/shed-cli-tool/archive/1.19.1.tar.gz"
+  url "https://github.com/shedcollective/shed-cli-tool/archive/1.19.2.tar.gz"
 
   # Generate hash of the above file and put onto clipboard
-  # printf $(curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.19.1.tar.gz | shasum -a 256 | cut -c 1-64) | pbcopy
-  sha256 "2777b89e3a5826aa26909ab137579ec7b606e7706a2563932063b8c3ad10c919"
+  # printf $(curl -sL https://github.com/shedcollective/shed-cli-tool/archive/1.19.2.tar.gz | shasum -a 256 | cut -c 1-64) | pbcopy
+  sha256 "6a7f25bdbb417d12061ddcb8c684db9d7c996883ff22898a29425c67d49f1cb5"
 
   # Specify dependencies
   depends_on "php" => ">=8.1"
@@ -19,7 +19,7 @@ class Shed < Formula
   end
 
   test do
-    assert_match "Shed Command Line Tool 1.19.1", shell_output("#{bin}/shed --version")
+    assert_match "Shed Command Line Tool 1.19.2", shell_output("#{bin}/shed --version")
   end
 
 end
